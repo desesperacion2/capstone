e367399e56d90afa128b906bc26b1809d5224e41
Suazo <emilioestebansuazo@gmail.com>
2024-09-27 17:00:49 -0300

    prueba 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h1>Bienvenido a la página de inicio</h1>
       <p>Esta es la página principal.</p>
+      <p> Si leo esto funciona se como usar la conexion con github.</p>
     </div>
   );
 };
